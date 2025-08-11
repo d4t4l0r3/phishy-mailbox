20c68cef6aeaab498354873c29c13a7f450c50d5
datalore <d4t4.l0r3@protonmail.ch>
2025-08-11 14:13:24 +0200

    Added InputField for scheduledTime to email editor

--- src/app/admin/(loggedin)/emails/[id]/page.tsx
+++ src/app/admin/(loggedin)/emails/[id]/page.tsx
@@ -64,6 +64,7 @@ export default function Page({params: {id}}: {params: {id: string}}) {
       senderName: '',
       allowExternalImages: false,
       backofficeIdentifier: '',
+			scheduledTime: 0,
     },
   });
   const addMail = trpc.email.add.useMutation();
@@ -115,7 +116,10 @@ export default function Page({params: {id}}: {params: {id: string}}) {
       </Headline>
       <Form builder={builder} onSubmit={onSubmit}>
         <div className='my-2 flex flex-col gap-x-8 gap-y-2'>
-          <InputField label={t('identifier')} on={builder.fields.backofficeIdentifier} rules={{required: true}} />
+					<div className='flex gap-4'>
+						<InputField label={t('identifier')} on={builder.fields.backofficeIdentifier} rules={{required: true}} className='mr-2' />
+						<InputField label={t('scheduledTime')} on={builder.fields.scheduledTime} rules={{min: 0}} />
+					</div>
           <fieldset className='flex flex-wrap'>
             <legend className='text-md font-semibold'>{t('sender')}</legend>
             <div className='flex gap-4'>
